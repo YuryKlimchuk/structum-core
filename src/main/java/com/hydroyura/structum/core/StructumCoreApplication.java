6083ce86bdfd3b1f2aa46269a0df26172bc6e93e
yury.klimchuk <hydro.yura@gmail.com>
2026-02-14 21:25:10 +0300

    init commit

---
+++ src/main/java/com/hydroyura/structum/core/StructumCoreApplication.java
@@ -0,0 +1,13 @@
+package com.hydroyura.structum_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructumCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructumCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hydroyura/structum/core/StructumCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydroyura.structum_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructumCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
